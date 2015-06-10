a63953396cb05d8e1fceb84f0029e69748f85914
Itay Amar <itayamar@gmail.com>
2015-06-10 09:43:07 +0300

    Added animations via angular

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // Declare app level module which depends on views, and components
 var myApp=angular.module('myApp', [
+  'ngAnimate',
   'ngRoute',
   'myApp.personal_details',
   'myApp.education',
